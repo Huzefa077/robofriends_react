6cf071efd467d963e0a1e765316884a105313b9f
Huzefa077 <huzaifasajid.sheikh@gmail.com>
2026-02-17 10:05:29 +0300

    fix favicon

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Scroll = (props) => {
     return(
-        <div style= {{ overflowY: 'scroll', border: '3px solid black', height: '500px' }} >
+        <div style= {{ overflowY: 'scroll', border: '3px solid black', height: '37.8rem' }} >
             {props.children}
         </div>
     );
